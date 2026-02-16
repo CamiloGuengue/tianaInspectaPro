8eda1d36be2de64aec7069fba2453ee82362ef2b
CamiloGuengue <guenguec@gmail.com>
2026-02-16 10:06:18 -0500

    fix file 5 name

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/home/guengue/Escritorio/Docusaurus/HU1M4/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/home/guengue/Escritorio/Docusaurus/HU1M4/node_modules/infima/dist/css/default/default.css"),
+  require("/home/guengue/Escritorio/Docusaurus/HU1M4/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/guengue/Escritorio/Docusaurus/HU1M4/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/guengue/Escritorio/Docusaurus/HU1M4/src/css/custom.css"),
+];
